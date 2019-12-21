62fdd27893db2498ee6ae5e360c284d16be304db
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-12-22 00:19:19 +0500

    Initial commit

--- src/behaviors/NestedSetsQueryBehavior.php
+++ src/behaviors/NestedSetsQueryBehavior.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @link https://github.com/creocoder/yii2-nested-sets
- * @copyright Copyright (c) 2015 Alexander Kochetov
- * @license http://opensource.org/licenses/BSD-3-Clause
- */
-
 namespace kilyakus\nestedsets\behaviors;
 
 use yii\base\Behavior;
